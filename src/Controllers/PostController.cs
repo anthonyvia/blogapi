59c24b14d366ed30f2e01011e47dd53283561af1
Anthony Via <anthony.via@logos.com>
2014-09-07 10:59:25 -0700

    Create post controller.

---
+++ src/Controllers/PostController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace blogapi.Controllers
+{
+    public class PostController : Controller
+    {
+        public ActionResult CraetePost()
+        {
+        }
+    }
+}
