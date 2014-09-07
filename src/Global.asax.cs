c063163a9f6653371a2a1750fbea5d5718bb6b09
Anthony Via <anthony.via@logos.com>
2014-09-07 10:50:41 -0700

    Remove unnecessary files.

--- src/Global.asax.cs
+++ src/Global.asax.cs
@@ -21,7 +21,6 @@ namespace blogapi
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
