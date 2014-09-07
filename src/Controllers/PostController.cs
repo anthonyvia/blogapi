0004d25553dfbd7d7cfcffc06a4615522e425078
Anthony Via <anthony.via@logos.com>
2014-09-07 11:11:51 -0700

    Add basic error checking to CreatePost route.

--- src/Controllers/PostController.cs
+++ src/Controllers/PostController.cs
@@ -1,11 +1,22 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using blogapi.Models;
 
 namespace blogapi.Controllers
 {
-    public class PostController : Controller
+    public class PostController : ApiController
     {
-        public ActionResult CraetePost()
+        public HttpResponseMessage CreatePost(Post post = null)
         {
+            if (post == null)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "post cannot be null");
+            if (string.IsNullOrWhiteSpace(post.Body))
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "post body cannot be empty");
+            if (string.IsNullOrWhiteSpace(post.Title))
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "post title cannot be empty");
+
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "create post not allowed");
         }
     }
 }
---
+++ src/Models/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace blogapi.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
