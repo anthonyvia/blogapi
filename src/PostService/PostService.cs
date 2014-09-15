e3aa55b62da05708c03fd29d0931bde0a3ac7e01
Anthony Via <anthony.via@logos.com>
2014-09-14 18:31:21 -0700

    Add initial posts schema.

--- src/PostService/PostService.cs
+++ src/PostService/PostService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Data;
 using Common;
+using Dapper;
+using MySql.Data.MySqlClient;
 using PostService.Db;
+using PostService.Properties;
 
 namespace PostService
 {
@@ -14,6 +18,16 @@ namespace PostService
             DbPost dbPost = new DbPost(post.Title, post.Body);
 
             // TODO: Add MySQL connection/query
+            string query = @"INSERT INTO posts.posts (body, title) VALUES ('@body', '@title');";
+
+            using (IDbConnection connection = new MySqlConnection(Settings.Default.MysqlConnectionString))
+            {
+                connection.Execute(query, new
+                {
+                    body = dbPost.Body,
+                    title = dbPost.Title
+                });
+            }
 
             return new Post(dbPost.Title, dbPost.Body);
         }
