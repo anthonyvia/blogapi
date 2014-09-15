65eb091713c7438e1df46515502a4bd4b0b59e52
Anthony Via <anthony.via@logos.com>
2014-09-14 21:32:11 -0700

    Fix connection string.

--- src/PostService/Properties/Settings.Designer.cs
+++ src/PostService/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace PostService.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("server=127.0.0.1; database=blog; uid=root;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=127.0.0.1; database=posts; uid=root;")]
         public string MysqlConnectionString {
             get {
                 return ((string)(this["MysqlConnectionString"]));
